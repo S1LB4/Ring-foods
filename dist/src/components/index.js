ceb8dbc42eadd0c7c8eb392f7afeb65a088f1122
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-03-28 08:00:23 -0500

    Update index.js

--- dist/src/components/index.js
+++ dist/src/components/index.js
@@ -1,8 +1,5 @@
 export {default as navBar} from "../components/nav-bar/nav-bar.js"
 export {default as headerinicio} from "../components/nav-bar/headerinicio.js"
-export {default as contenedor} from "../components/nav-bar/contenedor.js"
 export {default as gallery1} from "../components/nav-bar/gallery.js"
 export {default as gallery} from "../components/nav-bar/gallery.js"
 export {default as light} from "../components/nav-bar/light.js"
-export {default as expertos} from "../components/nav-bar/experts.js"
-export {default as footer} from "../components/nav-bar/footer.js"
